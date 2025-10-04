106a353f468f4e4143f933dbea0354acd6cda869
MatsueSS <newmailnoways199@gmail.com>
2025-10-04 13:24:47 +0300

    for last commit

--- load/main.cpp
+++ load/main.cpp
@@ -6,11 +6,10 @@
 
 int main(void)
 {
-    // PostgresDB db;
-    // db.connect(get_conn());
-    // db.execute(std::string("CREATE TABLE cards (id SERIAL PRIMARY KEY, title TEXT NOT NULL, price INT, discount INT, date DATE);"), std::vector<std::string>{});
-    // db.execute(std::string("CREATE TABLE users (id TEXT, cards TEXT[]);"), std::vector<std::string>{});
+    PostgresDB db;
+    db.connect(get_conn());
+    db.execute(std::string("CREATE TABLE cards (id SERIAL PRIMARY KEY, title TEXT NOT NULL, price INT, discount INT, date DATE);"), std::vector<std::string>{});
+    db.execute(std::string("CREATE TABLE users (id TEXT, cards TEXT[]);"), std::vector<std::string>{});
 
-    // std::ofstream file("../.env");
-    
+    std::ofstream file("../.env");
 }
